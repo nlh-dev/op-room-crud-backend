5150872bf3b9be7913b3a22d02c203614055f3b1
nlh-dev <hnavarro331@gmail.com>
2024-09-21 09:33:14 -0400

    users and patients crud operations created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { PatientsModule } from './patients/patients.module';
 
 
 @Module({
-  imports: [],
+  imports: [UsersModule, PatientsModule],
   controllers: [],
   providers: [],
 })
