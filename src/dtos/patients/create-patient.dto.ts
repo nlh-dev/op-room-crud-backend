7e0613e13d0991d57975f0029b828e10f0aee2a7
Eduardo <eduardokunmixyeah@gmail.com>
2024-09-30 03:45:03 -0400

    complete edit patients

--- src/dtos/patients/create-patient.dto.ts
+++ src/dtos/patients/create-patient.dto.ts
@@ -1,1 +1,21 @@
-export class CreatePatientDto {}
+import { Transform } from "class-transformer";
+import { IsDate, IsString } from "class-validator";
+
+export class CreatePatientDto {
+    @IsString()
+    fullName: string;
+    @IsString()
+    speciality: string;
+    @Transform(({ value }) => new Date(value))
+    @IsDate()
+    date_start: Date;
+    @IsString()
+    state: string;
+}
+
+export class UpdatePatientDto {
+    @Transform(({ value }) => new Date(value))
+    @IsDate()
+    date_end: Date;
+}
+
--- src/patients/patients.service.ts
+++ src/patients/patients.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { CreatePatientDto } from '../dtos/patients/create-patient.dto';
-import { UpdatePatientDto } from '../dtos/patients/update-patient.dto';
+import { CreatePatientDto, UpdatePatientDto } from '../dtos/patients/create-patient.dto';
 import { BaseService } from 'src/base/base.service';
 import { patients, surgery_states } from '@prisma/client';
+import { DtoBaseResponse } from 'src/dtos/base-response.dto';
+import { badResponse, baseResponse } from 'src/dtos/baseResponse';
 
 @Injectable()
 export class PatientsService extends BaseService {
-  
+
   async findAll(): Promise<patients[]> {
     return await this.prismaService.patients.findMany({
       include: {
@@ -42,19 +43,80 @@ export class PatientsService extends BaseService {
     });
   }
 
+  async getStatesPatient(): Promise<surgery_states[]> {
+    return await this.prismaService.surgery_states.findMany();
+  }
+
   findOne(id: number) {
     return `This action returns a #${id} patient`;
   }
 
-  create(createPatientDto: CreatePatientDto) {
-    return 'This action adds a new patient';
+  async create(createPatientDto: CreatePatientDto): Promise<DtoBaseResponse> {
+    try {
+      await this.prismaService.patients.create({
+        data: {
+          patients_name: createPatientDto.fullName,
+          patients_started_date: createPatientDto.date_start,
+          patients_surgery_state_id: Number(createPatientDto.state),
+          patients_surgery_type_id: Number(createPatientDto.speciality)
+        }
+      })
+      baseResponse.message = 'Paciente agregado exitosamente.'
+      return baseResponse;
+    } catch (err) {
+      return badResponse;
+    }
   }
 
-  update(id: number, updatePatientDto: UpdatePatientDto) {
-    return `This action updates a #${id} patient`;
+  async update(id: number, updatePatientDto: CreatePatientDto): Promise<DtoBaseResponse> {
+    try{
+      await this.prismaService.patients.update({
+        data:{
+          patients_name: updatePatientDto.fullName,
+          patients_started_date: updatePatientDto.date_start,
+          patients_surgery_state_id: Number(updatePatientDto.state),
+          patients_surgery_type_id: Number(updatePatientDto.speciality)
+        },
+        where: {
+          patients_id: id
+        }
+      })
+      baseResponse.message = 'Paciente actualizado exitosamente.'
+      return baseResponse;
+    } catch(err) {
+      return badResponse;
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} patient`;
+  async updateDate(id: number, updatePatientDto: UpdatePatientDto): Promise<DtoBaseResponse> {
+    try{
+      await this.prismaService.patients.update({
+        data:{
+          patients_updated_date: updatePatientDto.date_end,
+        },
+        where: {
+          patients_id: id
+        }
+      })
+      baseResponse.message = 'Paciente actualizado exitosamente.'
+      return baseResponse;
+    } catch(err) {
+      return badResponse;
+    }
+  }
+
+  async remove(id: number): Promise<DtoBaseResponse> {
+    try {
+      await this.prismaService.patients.delete({
+        where: {
+          patients_id: id
+        }
+      })
+      baseResponse.message = 'Paciente eliminado exitosamente.'
+      return baseResponse;
+    } catch (err) {
+      badResponse.message = 'No se pudo eliminar el paciente.'
+      return badResponse;
+    }
   }
 }
